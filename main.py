1795afca5737bfc5f7986726f28e93794b3a9c8b
NaoakiUmedu <n.umezu1014@gmail.com>
2021-01-03 22:58:51 +0900

    lesson 5 finished

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Python Study
-# Next--->5
+# Next--->6
 
-print("Add GitHub")
+string1, string2, string3 = '', 'Trondheim', 'Hammer Dance'
+non_null = string1 or string2 or string3
+print(non_null)
